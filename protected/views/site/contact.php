bbcef3fa35a72991b1b72356280379ee9859a3a3
Olmo Jiménez <olmojial@gmail.com>
2013-08-03 19:45:06 +0200

    - Añadido css del formulario.

--- protected/views/site/contact.php
+++ protected/views/site/contact.php
@@ -59,52 +59,54 @@ $this->breadcrumbs=array(
             ),
         )); ?>
 
-        <?php echo $form->errorSummary($model); ?>
+        <div class="form">
 
-        <div class="one_half first">
-            <?php echo $form->labelEx($model,'name'); ?>
-            <?php echo $form->textField($model,'name'); ?>
-            <?php echo $form->error($model,'name'); ?>
-        </div>
+            <?php echo $form->errorSummary($model); ?>
 
-        <div class="one_half last">
-            <?php echo $form->labelEx($model,'email'); ?>
-            <?php echo $form->textField($model,'email'); ?>
-            <?php echo $form->error($model,'email'); ?>
-        </div>
-
-        <div class="row">
-            <?php echo $form->labelEx($model,'subject'); ?>
-            <?php echo $form->textField($model,'subject',array('size'=>60,'maxlength'=>128)); ?>
-            <?php echo $form->error($model,'subject'); ?>
-        </div>
-
-        <div class="row">
-            <?php echo $form->labelEx($model,'body'); ?>
-            <?php echo $form->textArea($model,'body',array('rows'=>6, 'cols'=>50)); ?>
-            <?php echo $form->error($model,'body'); ?>
-        </div>
-
-        <?php if(CCaptcha::checkRequirements()): ?>
-            <div class="row">
-                <?php echo $form->labelEx($model,'verifyCode'); ?>
-                <div>
-                    <?php $this->widget('CCaptcha'); ?>
-                    <?php echo $form->textField($model,'verifyCode'); ?>
-                </div>
-                <div class="hint">Please enter the letters as they are shown in the image above.
-                    <br/>Letters are not case-sensitive.</div>
-                <?php echo $form->error($model,'verifyCode'); ?>
+            <div class="one_half first">
+                <?php echo $form->labelEx($model,'name'); ?>
+                <?php echo $form->textField($model,'name'); ?>
+                <?php echo $form->error($model,'name'); ?>
             </div>
-        <?php endif; ?>
 
-        <div>
-            <?php echo CHtml::submitButton('Submit', array('class' => 'button')); ?>
-            <!-- <p class="status"></p> -->
+            <div class="one_half last">
+                <?php echo $form->labelEx($model,'email'); ?>
+                <?php echo $form->textField($model,'email'); ?>
+                <?php echo $form->error($model,'email'); ?>
+            </div>
+
+            <div class="row">
+                <?php echo $form->labelEx($model,'subject'); ?>
+                <?php echo $form->textField($model,'subject',array('size'=>60,'maxlength'=>128)); ?>
+                <?php echo $form->error($model,'subject'); ?>
+            </div>
+
+            <div class="row">
+                <?php echo $form->labelEx($model,'body'); ?>
+                <?php echo $form->textArea($model,'body',array('rows'=>6, 'cols'=>50)); ?>
+                <?php echo $form->error($model,'body'); ?>
+            </div>
+
+            <?php if(CCaptcha::checkRequirements()): ?>
+                <div class="row">
+                    <?php echo $form->labelEx($model,'verifyCode'); ?>
+                    <div>
+                        <?php $this->widget('CCaptcha'); ?>
+                        <?php echo $form->textField($model,'verifyCode'); ?>
+                    </div>
+                    <div class="hint">Please enter the letters as they are shown in the image above.
+                        <br/>Letters are not case-sensitive.</div>
+                    <?php echo $form->error($model,'verifyCode'); ?>
+                </div>
+            <?php endif; ?>
+
+            <div>
+                <?php echo CHtml::submitButton('Submit', array('class' => 'button')); ?>
+                <!-- <p class="status"></p> -->
+            </div>
+
+            <?php $this->endWidget(); ?>
         </div>
-
-        <?php $this->endWidget(); ?>
-
 
 <?php endif; ?>
 
