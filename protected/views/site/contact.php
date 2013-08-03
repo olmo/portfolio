7b18be4b147663452cc074be2b2056a22e3d4af1
Francisco Carranza Garcia <oz.carranza@gmail.com>
2013-08-03 19:58:00 +0200

    Ultimas traducciones del formulario de contacto.

--- protected/views/site/contact.php
+++ protected/views/site/contact.php
@@ -94,8 +94,8 @@ $this->breadcrumbs=array(
                         <?php $this->widget('CCaptcha'); ?>
                         <?php echo $form->textField($model,'verifyCode'); ?>
                     </div>
-                    <div class="hint">Please enter the letters as they are shown in the image above.
-                        <br/>Letters are not case-sensitive.</div>
+                    <div class="hint">Por favor, introduzca las letras que se muestran en la imagen de arriba.
+                        <br/>Las letras no distinguen entre mayúsculas y minúsculas.</div>
                     <?php echo $form->error($model,'verifyCode'); ?>
                 </div>
             <?php endif; ?>
