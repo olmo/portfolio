44357c0fa1a0ebdd9fefed5cc24bcc5681cae04a
Francisco Carranza Garcia <oz.carranza@gmail.com>
2013-08-03 17:52:47 +0200

    Contact form.

--- protected/views/site/contact.php
+++ protected/views/site/contact.php
@@ -3,83 +3,102 @@
 /* @var $model ContactForm */
 /* @var $form CActiveForm */
 
-$this->pageTitle=Yii::app()->name . ' - Contact Us';
+$this->pageTitle=Yii::app()->name . ' - Contacto';
 $this->breadcrumbs=array(
 	'Contact',
 );
 ?>
 
-<h1>Contact Us</h1>
+<section class="layout contact right"><div class="skew"></div>
 
-<?php if(Yii::app()->user->hasFlash('contact')): ?>
+    <h1>Contacto</h1>
 
-<div class="flash-success">
-	<?php echo Yii::app()->user->getFlash('contact'); ?>
-</div>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque at turpis nisi, vitae ullamcorper elit. Proin rhoncus bibendum sollicitudin. Mauris interdum, erat vitae dignissim placerat, nunc elit vehicula elit, in dignissim lacus leo at dolor.</p>
 
-<?php else: ?>
+    <br>
 
-<p>
-If you have business inquiries or other questions, please fill out the following form to contact us. Thank you.
-</p>
+    <!-- Contact information -->
+    <p class="column">
+        <strong>Oficina:</strong><br>
+        Calle Primavera 8<br>
+        Granada, España
+    </p>
+    <p class="column">
+        <strong>Contacto:</strong><br>
+        info@decorvinilo.com<br>
+        607 53 53 35
+    </p>
 
-<div class="form">
+    <div class="clearfix"></div>
+    <br>
 
-<?php $form=$this->beginWidget('CActiveForm', array(
-	'id'=>'contact-form',
-	'enableClientValidation'=>true,
-	'clientOptions'=>array(
-		'validateOnSubmit'=>true,
-	),
-)); ?>
+    <h3>Mandenos un e-mail</h3>
 
-	<p class="note">Fields with <span class="required">*</span> are required.</p>
+    <p class="note">Campos con <span class="required">*</span> son obligatorios.</p>
 
-	<?php echo $form->errorSummary($model); ?>
+    <!-- Contact form -->
+    <?php if(Yii::app()->user->hasFlash('contact')): ?>
 
-	<div class="row">
-		<?php echo $form->labelEx($model,'name'); ?>
-		<?php echo $form->textField($model,'name'); ?>
-		<?php echo $form->error($model,'name'); ?>
-	</div>
+        <div class="flash-success">
+            <?php echo Yii::app()->user->getFlash('contact'); ?>
+        </div>
 
-	<div class="row">
-		<?php echo $form->labelEx($model,'email'); ?>
-		<?php echo $form->textField($model,'email'); ?>
-		<?php echo $form->error($model,'email'); ?>
-	</div>
+    <?php else: ?>
 
-	<div class="row">
-		<?php echo $form->labelEx($model,'subject'); ?>
-		<?php echo $form->textField($model,'subject',array('size'=>60,'maxlength'=>128)); ?>
-		<?php echo $form->error($model,'subject'); ?>
-	</div>
+    <form action="#" id="contact_form">
 
-	<div class="row">
-		<?php echo $form->labelEx($model,'body'); ?>
-		<?php echo $form->textArea($model,'body',array('rows'=>6, 'cols'=>50)); ?>
-		<?php echo $form->error($model,'body'); ?>
-	</div>
+        <?php $form=$this->beginWidget('CActiveForm', array(
+            'id'=>'contact-form',
+            'enableClientValidation'=>true,
+            'clientOptions'=>array(
+                'validateOnSubmit'=>true,
+            ),
+        )); ?>
 
-	<?php if(CCaptcha::checkRequirements()): ?>
-	<div class="row">
-		<?php echo $form->labelEx($model,'verifyCode'); ?>
-		<div>
-		<?php $this->widget('CCaptcha'); ?>
-		<?php echo $form->textField($model,'verifyCode'); ?>
-		</div>
-		<div class="hint">Please enter the letters as they are shown in the image above.
-		<br/>Letters are not case-sensitive.</div>
-		<?php echo $form->error($model,'verifyCode'); ?>
-	</div>
-	<?php endif; ?>
+        <?php echo $form->errorSummary($model); ?>
 
-	<div class="row buttons">
-		<?php echo CHtml::submitButton('Submit'); ?>
-	</div>
+        <div class="one_half first">
+            <?php echo $form->labelEx($model,'name'); ?>
+            <?php echo $form->textField($model,'name'); ?>
+            <?php echo $form->error($model,'name'); ?>
+        </div>
 
-<?php $this->endWidget(); ?>
+        <div class="one_half last">
+            <?php echo $form->labelEx($model,'email'); ?>
+            <?php echo $form->textField($model,'email'); ?>
+            <?php echo $form->error($model,'email'); ?>
+        </div>
 
-</div><!-- form -->
+        <div class="row">
+            <?php echo $form->labelEx($model,'subject'); ?>
+            <?php echo $form->textField($model,'subject',array('size'=>60,'maxlength'=>128)); ?>
+            <?php echo $form->error($model,'subject'); ?>
+        </div>
+
+        <div class="row">
+            <?php echo $form->labelEx($model,'body'); ?>
+            <?php echo $form->textArea($model,'body',array('rows'=>6, 'cols'=>50)); ?>
+            <?php echo $form->error($model,'body'); ?>
+        </div>
+
+        <?php if(CCaptcha::checkRequirements()): ?>
+            <div class="row">
+                <?php echo $form->labelEx($model,'verifyCode'); ?>
+                <div>
+                    <?php $this->widget('CCaptcha'); ?>
+                    <?php echo $form->textField($model,'verifyCode'); ?>
+                </div>
+                <div class="hint">Please enter the letters as they are shown in the image above.
+                    <br/>Letters are not case-sensitive.</div>
+                <?php echo $form->error($model,'verifyCode'); ?>
+            </div>
+        <?php endif; ?>
+
+        <div class="button">
+            <?php echo CHtml::submitButton('Enviar'); ?>
+            <!-- <p class="status"></p> -->
+        </div>
+
+        <?php $this->endWidget(); ?>
 
 <?php endif; ?>
