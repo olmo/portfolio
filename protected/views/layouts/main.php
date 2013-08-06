0749523b24b3a8e436b84e7c542da1ba3f6a5693
Olmo Jiménez <olmojial@gmail.com>
2013-08-06 03:09:59 +0200

    Añadidas imágenes que faltaban.

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -25,6 +25,8 @@
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/style.css" />
     <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/form.css" />
 
+    <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1">
+
 	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
 </head>
 
