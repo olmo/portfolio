a6f32c55298395032ddc46f5dea1520929c3c774
Olmo Jiménez <olmojial@gmail.com>
2013-10-19 18:41:47 +0200

    .

---
+++ protected/views/layouts/home.php
@@ -0,0 +1,8 @@
+<?php /* @var $this Controller */ ?>
+<?php $this->beginContent('//layouts/main'); ?>
+<div id="content" class="content full">
+    <div class="container">
+        <?php echo $content; ?>
+    </div>
+</div>
+<?php $this->endContent(); ?>
