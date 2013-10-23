2cfe3f6149cce6809ec35824a7da8c6f4fa4f457
Olmo Jiménez <olmojial@gmail.com>
2013-10-23 23:18:58 +0200

    .

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -249,7 +249,7 @@
 <script src="<?php echo Yii::app()->request->baseUrl; ?>/vendor/circle-flip-slideshow/js/jquery.flipshow.js"></script>
 <script src="<?php echo Yii::app()->request->baseUrl; ?>/vendor/magnific-popup/magnific-popup.js"></script>
 <script src="<?php echo Yii::app()->request->baseUrl; ?>/vendor/jquery.validate.js"></script>
-<script src="<?php echo Yii::app()->request->baseUrl; ?>/vendor/jquery.isotope.js"></script>
+<script src="<?php echo Yii::app()->request->baseUrl; ?>/vendor/isotope/jquery.isotope.js"></script>
 
 <script src="<?php echo Yii::app()->request->baseUrl; ?>/js/plugins.js"></script>
 
@@ -261,21 +261,6 @@
 
 <!-- Custom JS -->
 <script src="<?php echo Yii::app()->request->baseUrl; ?>/js/custom.js"></script>
-
-<!-- Google Analytics: Change UA-XXXXX-X to be your site's ID. Go to http://www.google.com/analytics/ for more information. -->
-<!--
-<script>
-    var _gaq = _gaq || [];
-    _gaq.push(['_setAccount', 'UA-XXXXX-X']);
-    _gaq.push(['_trackPageview']);
-
-    (function() {
-        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-    })();
-</script>
--->
 
 </body>
 
