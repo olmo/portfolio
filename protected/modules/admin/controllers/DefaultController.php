0b677ed7e361e2cb9fb07009f482c571ff2d1bc3
Francisco Carranza <oz.carranza@gmail.com>
2013-10-27 19:55:04 +0100

    access rules

--- protected/modules/admin/controllers/DefaultController.php
+++ protected/modules/admin/controllers/DefaultController.php
@@ -4,10 +4,48 @@ class DefaultController extends CController
 {
     // public $layout='//layouts/login';
 
-	public function actionIndex()
-	{
-        $this->layout = 'login';
+    /**
+     * Specifies the access control rules.
+     * This method is used by the 'accessControl' filter.
+     * @return array access control rules
+     */
+    public function accessRules()
+    {
+        return array(
+            array('allow',
+                'actions'=>array('index'),
+                'users'=>array('admin'),
+            ),
+        );
+    }
 
+	public function actionIndex()
+    {
 		$this->render('index');
 	}
+
+    public function actionLogin()
+    {
+        $this->layout = 'login';
+
+        $model=new LoginForm;
+
+        // if it is ajax validation request
+        if(isset($_POST['ajax']) && $_POST['ajax']==='login-form')
+        {
+            echo CActiveForm::validate($model);
+            Yii::app()->end();
+        }
+
+        // collect user input data
+        if(isset($_POST['LoginForm']))
+        {
+            $model->attributes=$_POST['LoginForm'];
+            // validate user input and redirect to the previous page if valid
+            if($model->validate() && $model->login())
+                $this->redirect(Yii::app()->user->returnUrl);
+        }
+        // display the login form
+        $this->render('login',array('model'=>$model));
+    }
 }
