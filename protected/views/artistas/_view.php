6ae99b363a730e443cec6b921b44bfe3710f38ae
Francisco Carranza <oz.carranza@gmail.com>
2013-10-30 16:03:09 +0100

    Quitado redes sociales artistas.

--- protected/views/artistas/_view.php
+++ protected/views/artistas/_view.php
@@ -15,15 +15,6 @@
                 <span class="thumb-info-type"><?php echo CHtml::encode($data->idCategoria->nombre); ?></span>
             </span>
         </a>
-
-        <span class="thumb-info-caption">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ac ligula mi, non suscipitaccumsan.</p>
-            <span class="thumb-info-social-icons">
-                <a rel="tooltip" data-placement="bottom" target="_blank" href="http://www.facebook.com" data-original-title="Facebook"><i class="icon-facebook"></i><span>Facebook</span></a>
-                <a rel="tooltip" data-placement="bottom" href="http://www.twitter.com" data-original-title="Twitter"><i class="icon-twitter"></i><span>Twitter</span></a>
-                <a rel="tooltip" data-placement="bottom" href="http://www.linkedin.com" data-original-title="Linkedin"><i class="icon-linkedin"></i><span>Linkedin</span></a>
-            </span>
-        </span>
     </div>
 </li>
 
