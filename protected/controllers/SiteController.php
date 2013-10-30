5aa3c6b38a5548ecac75b4ec406f45ba3952731d
Francisco Carranza <oz.carranza@gmail.com>
2013-10-30 03:07:57 +0100

    Home.

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -28,11 +28,12 @@ class SiteController extends Controller
 	 * when an action is not explicitly requested by users.
 	 */
 	public function actionIndex()
-{
-    // renders the view file 'protected/views/site/index.php'
-    // using the default layout 'protected/views/layouts/main.php'
-    $this->render('index');
-}
+    {
+        $this->titulo = 'Inicio';
+        // renders the view file 'protected/views/site/index.php'
+        // using the default layout 'protected/views/layouts/main.php'
+        $this->render('index');
+    }
 
     public function actionAbout()
     {
--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -1,13 +1,297 @@
 <?php
 /* @var $this SiteController */
 
-$this->pageTitle=Yii::app()->name . ' - Inicio';;
+$this->pageTitle = Yii::app()->name . ' - Inicio';
 ?>
 
-<h1><?php echo CHtml::encode(Yii::app()->name); ?></h1>
+<div role="main" class="main">
+<div id="content" class="content full">
 
-<p>
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque at turpis nisi, vitae
-    ullamcorper elit. Proin rhoncus bibendum sollicitudin. Mauris interdum, erat vitae
-    dignissim placerat, nunc elit vehicula elit, in dignissim lacus leo at dolor.
-</p>
+<div class="body">
+<div class="slider-container">
+    <div class="slider" id="revolutionSlider">
+        <ul>
+            <li data-transition="fade" data-slotamount="10" data-masterspeed="300">
+                <img src="<?php echo Yii::app()->request->baseUrl; ?>/img/slides/slide-bg.jpg" data-fullwidthcentering="on" alt="">
+
+                <div class="caption sft stb visible-desktop"
+                     data-x="42"
+                     data-y="180"
+                     data-speed="300"
+                     data-start="1000"
+                     data-easing="easeOutExpo"><img src="<?php echo Yii::app()->request->baseUrl; ?>/img/slides/slide-title-border.png" alt=""></div>
+
+                <div class="caption top-label lfl stl"
+                     data-x="92"
+                     data-y="180"
+                     data-speed="300"
+                     data-start="500"
+                     data-easing="easeOutExpo">LO REM IPSU A TINDSI
+                </div>
+
+                <div class="caption sft stb visible-desktop"
+                     data-x="342"
+                     data-y="180"
+                     data-speed="300"
+                     data-start="1000"
+                     data-easing="easeOutExpo"><img src="<?php echo Yii::app()->request->baseUrl; ?>/img/slides/slide-title-border.png" alt=""></div>
+
+                <div class="caption main-label sft stb"
+                     data-x="0"
+                     data-y="230"
+                     data-speed="300"
+                     data-start="1500"
+                     data-easing="easeOutExpo">AC TINCINT?
+                </div>
+
+                <div class="caption bottom-label sft stb"
+                     data-x="50"
+                     data-y="280"
+                     data-speed="500"
+                     data-start="2000"
+                     data-easing="easeOutExpo">Consectetur adipiscing elit.
+                </div>
+
+                <div class="caption randomrotate"
+                     data-x="800"
+                     data-y="250"
+                     data-speed="500"
+                     data-start="2500"
+                     data-easing="easeOutBack"><img src="<?php echo Yii::app()->request->baseUrl; ?>/img/slides/slide-concept-2-1.png" alt=""></div>
+
+                <div class="caption sfb"
+                     data-x="850"
+                     data-y="200"
+                     data-speed="400"
+                     data-start="3000"
+                     data-easing="easeOutBack"><img src="<?php echo Yii::app()->request->baseUrl; ?>/img/slides/slide-concept-2-2.png" alt=""></div>
+
+                <div class="caption sfb"
+                     data-x="820"
+                     data-y="170"
+                     data-speed="700"
+                     data-start="3150"
+                     data-easing="easeOutBack"><img src="<?php echo Yii::app()->request->baseUrl; ?>/img/slides/slide-concept-2-3.png" alt=""></div>
+
+                <div class="caption sfb"
+                     data-x="770"
+                     data-y="130"
+                     data-speed="1000"
+                     data-start="3250"
+                     data-easing="easeOutBack"><img src="<?php echo Yii::app()->request->baseUrl; ?>/img/slides/slide-concept-2-4.png" alt=""></div>
+
+                <div class="caption sfb"
+                     data-x="500"
+                     data-y="80"
+                     data-speed="600"
+                     data-start="3450"
+                     data-easing="easeOutExpo"><img src="<?php echo Yii::app()->request->baseUrl; ?>/img/slides/slide-concept-2-5.png" alt=""></div>
+
+                <div class="caption blackboard-text lfb "
+                     data-x="530"
+                     data-y="300"
+                     data-speed="500"
+                     data-start="3450"
+                     data-easing="easeOutExpo" style="font-size: 37px;">Ltyhj
+                </div>
+
+                <div class="caption blackboard-text lfb "
+                     data-x="555"
+                     data-y="350"
+                     data-speed="500"
+                     data-start="3650"
+                     data-easing="easeOutExpo" style="font-size: 47px;">Otrsuhe
+                </div>
+
+                <div class="caption blackboard-text lfb "
+                     data-x="580"
+                     data-y="400"
+                     data-speed="500"
+                     data-start="3850"
+                     data-easing="easeOutExpo" style="font-size: 32px;">twr nhg :)
+                </div>
+            </li>
+            <li data-transition="fade" data-slotamount="10" data-masterspeed="300">
+                <img src="<?php echo Yii::app()->request->baseUrl; ?>/img/slides/slide-bg.jpg" data-fullwidthcentering="on" alt="">
+
+                <div class="caption fade"
+                     data-x="50"
+                     data-y="100"
+                     data-speed="2500"
+                     data-start="200"
+                     data-easing="easeOutExpo"><img src="<?php echo Yii::app()->request->baseUrl; ?>/img/slides/slide-concept.png" alt=""></div>
+
+                <div class="caption blackboard-text fade "
+                     data-x="180"
+                     data-y="180"
+                     data-speed="1500"
+                     data-start="1000"
+                     data-easing="easeOutExpo" style="font-size: 30px;">lorem to
+                </div>
+
+                <div class="caption blackboard-text fade "
+                     data-x="180"
+                     data-y="220"
+                     data-speed="1500"
+                     data-start="1200"
+                     data-easing="easeOutExpo" style="font-size: 40px;">ipsum!
+                </div>
+
+                <div class="caption main-label sft stb"
+                     data-x="580"
+                     data-y="200"
+                     data-speed="300"
+                     data-start="1500"
+                     data-easing="easeOutExpo">LOREM IPSUM!
+                </div>
+
+                <div class="caption bottom-label sft stb"
+                     data-x="580"
+                     data-y="250"
+                     data-speed="500"
+                     data-start="2000"
+                     data-easing="easeOutExpo">Lorem ipsum dolor sit amet.
+                </div>
+
+            </li>
+        </ul>
+    </div>
+</div>
+
+<div class="home-intro">
+    <div class="container">
+
+        <div class="row">
+            <div class="span8">
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, <em> pellentesque. </em>
+                    <span>Nulla hendrerit arcu, ac tincidunt mauris lacus.</span>
+                </p>
+            </div>
+        </div>
+
+    </div>
+</div>
+
+<div class="container">
+
+    <div class="row center">
+        <div class="span12">
+            <h2 class="short"> Donec tellus massa, tristique <strong class="inverted"> condimentum </strong> vel. </h2>
+
+            <p class="featured lead">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce elementum, nulla vel pellentesque
+                consequat, ante nulla hendrerit arcu, ac tincidunt mauris lacus sed leo. vamus suscipit molestie
+                vestibulum.
+            </p>
+        </div>
+    </div>
+
+</div>
+
+<div class="home-concept">
+    <div class="container">
+
+        <div class="row center">
+            <span class="sun"></span>
+            <span class="cloud"></span>
+
+            <div class="span2 offset1">
+                <div class="process-image" data-appear-animation="bounceIn">
+                    <img src="<?php echo Yii::app()->request->baseUrl; ?>/img/home-concept-item-1.jpg" alt=""/>
+                    <strong>Estrategia</strong>
+                </div>
+            </div>
+            <div class="span2">
+                <div class="process-image" data-appear-animation="bounceIn" data-appear-animation-delay="200">
+                    <img src="<?php echo Yii::app()->request->baseUrl; ?>/img/home-concept-item-2.jpg" alt=""/>
+                    <strong>Planificación</strong>
+                </div>
+            </div>
+            <div class="span2">
+                <div class="process-image" data-appear-animation="bounceIn" data-appear-animation-delay="400">
+                    <img src="<?php echo Yii::app()->request->baseUrl; ?>/img/home-concept-item-3.jpg" alt=""/>
+                    <strong>Desarrollo</strong>
+                </div>
+            </div>
+            <div class="span4 offset1">
+                <div class="project-image">
+                    <div id="fcSlideshow" class="fc-slideshow">
+                        <ul class="fc-slides">
+                            <li><a href="portfolio-single-project.html"><img src="<?php echo Yii::app()->request->baseUrl; ?>/img/projects/project-home-1.jpg"/></a>
+                            </li>
+                            <li><a href="portfolio-single-project.html"><img src="<?php echo Yii::app()->request->baseUrl; ?>/img/projects/project-home-2.jpg"/></a>
+                            </li>
+                            <li><a href="portfolio-single-project.html"><img src="<?php echo Yii::app()->request->baseUrl; ?>/img/projects/project-home-3.jpg"/></a>
+                            </li>
+                        </ul>
+                    </div>
+                    <strong class="our-work">Nuestro trabajo</strong>
+                </div>
+            </div>
+        </div>
+
+        <hr class="tall"/>
+
+    </div>
+</div>
+
+<div class="container">
+
+    <div class="row center">
+        <div class="span12">
+            <h2 class="short">Donec tellus massa, tristique <strong> condimentum </strong> vel.</h2>
+            <h4 class="lead tall">Donec tellus massa, tristique sit amet condimentum vel, facilisis quis sapien.</h4>
+        </div>
+    </div>
+    <div class="row center">
+        <div class="flexslider unstyled"
+             data-plugin-options='{"directionNav":false, "animation":"slide", "slideshow": false, "maxVisibleItems": 6}'>
+            <ul class="slides">
+                <li>
+                    <div class="span2">
+                        <img class="mobile-max-width small" src="<?php echo Yii::app()->request->baseUrl; ?>/img/logo-client-1.jpg" alt="">
+                    </div>
+                </li>
+                <li>
+                    <div class="span2">
+                        <img class="mobile-max-width small" src="<?php echo Yii::app()->request->baseUrl; ?>/img/logo-client-2.jpg" alt="">
+                    </div>
+                </li>
+                <li>
+                    <div class="span2">
+                        <img class="mobile-max-width small" src="<?php echo Yii::app()->request->baseUrl; ?>/img/logo-client-3.jpg" alt="">
+                    </div>
+                </li>
+                <li>
+                    <div class="span2">
+                        <img class="mobile-max-width small" src="<?php echo Yii::app()->request->baseUrl; ?>/img/logo-client-4.jpg" alt="">
+                    </div>
+                </li>
+                <li>
+                    <div class="span2">
+                        <img class="mobile-max-width small" src="<?php echo Yii::app()->request->baseUrl; ?>/img/logo-client-5.jpg" alt="">
+                    </div>
+                </li>
+                <li>
+                    <div class="span2">
+                        <img class="mobile-max-width small" src="<?php echo Yii::app()->request->baseUrl; ?>/img/logo-client-6.jpg" alt="">
+                    </div>
+                </li>
+                <li>
+                    <div class="span2">
+                        <img class="mobile-max-width small" src="<?php echo Yii::app()->request->baseUrl; ?>/img/logo-client-4.jpg" alt="">
+                    </div>
+                </li>
+                <li>
+                    <div class="span2">
+                        <img class="mobile-max-width small" src="<?php echo Yii::app()->request->baseUrl; ?>/img/logo-client-2.jpg" alt="">
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
+</div>
+</div>
+</div>
