0cacad5d88481f5e5d4ca9d6fb57ebde3a81da6a
Olmo Jiménez <olmojial@gmail.com>
2014-01-04 16:53:27 +0100

    Añadidas preguntas frecuentes en español.

--- protected/views/site/faq.php
+++ protected/views/site/faq.php
@@ -11,14 +11,6 @@ $this->breadcrumbs=array(
 
     <h2><strong>Preguntas</strong> frecuentes </h2>
 
-    <div class="row">
-        <div class="span12">
-            <p class="lead">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque rutrum pellentesque imperdiet. Nulla lacinia iaculis nulla non pulvinar. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Ut eu risus enim, ut pulvinar lectus. Sed hendrerit nibh metus.
-            </p>
-        </div>
-    </div>
-
     <hr>
 
     <div class="row">
@@ -26,48 +18,53 @@ $this->breadcrumbs=array(
 
             <div class="toogle">
                 <section class="toggle active">
-                    <label>Curabitur eget leo at velit imperdiet vague iaculis vitaes?</label>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eget leo at velit imperdiet varius. In eu ipsum vitae velit congue iaculis vitae at risus. Nullam tortor nunc, bibendum vitae semper a, volutpat eget massa. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer fringilla, orci sit amet posuere auctor, orci eros pellentesque odio, nec pellentesque erat ligula nec massa. Aenean consequat lorem ut felis ullamcorper posuere gravida tellus faucibus. Maecenas dolor elit, pulvinar eu vehicula eu, consequat et lacus. Duis et purus ipsum. In auctor mattis ipsum id molestie. Donec risus nulla, fringilla a rhoncus vitae, semper a massa. Vivamus ullamcorper, enim sit amet consequat laoreet, tortor tortor dictum urna, ut egestas urna ipsum nec libero. Nulla justo leo, molestie vel tempor nec, egestas at massa. Aenean pulvinar, felis porttitor iaculis pulvinar, odio orci sodales odio, ac pulvinar felis quam sit.</p>
+                    <label>¿Qué tipo de producto ofrece Global Arte?</label>
+                    <p>Tenemos un amplio surtido de cuadros y murales en diferentes técnicas como Fotografía, Gráficos-ilustración, Arte digital, Acrílico y Pintura Original. Las ediciones de cada uno de los cuadros son limitadas y eso le garantiza la exclusividad de la obra.</p>
                 </section>
 
                 <section class="toggle">
-                    <label>Curabitur eget leo at velit imperdiet viaculis vitaes?</label>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eget leo at velit imperdiet varius. In eu ipsum vitae velit congue iaculis vitae at risus. Nullam tortor nunc, bibendum vitae semper a, volutpat eget massa.</p>
+                    <label>¿Cómo realizar el pedido?</label>
+                    <p>El pedido se realiza a través de la página web, contactando con nuestro correo de atención al cliente y solicitándonos la obra que desea, tamaño y presentación que previamente ha podido ver en la página. Nuestro equipo de atención al cliente contactará con usted para confirmar su petición.</p>
                 </section>
 
                 <section class="toggle">
-                    <label>Curabitur eget leo at velit imperdiet varius iaculis vitaes?</label>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eget leo at velit imperdiet varius. In eu ipsum vitae velit congue iaculis vitae at risus. Nullam tortor nunc, bibendum vitae semper a, volutpat eget massa. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer fringilla, orci sit amet posuere auctor, orci eros pellentesque odio, nec pellentesque erat ligula nec massa. Aenean consequat lorem ut felis ullamcorper posuere gravida tellus faucibus. Maecenas dolor elit, pulvinar eu vehicula eu, consequat et lacus. Duis et purus ipsum. In auctor mattis ipsum id molestie. Donec risus nulla, fringilla a rhoncus vitae, semper a massa. Vivamus ullamcorper, enim sit amet consequat laoreet, tortor tortor dictum urna, ut egestas urna ipsum nec libero. Nulla justo leo, molestie vel tempor nec, egestas at massa. Aenean pulvinar, felis porttitor iaculis pulvinar, odio orci sodales odio, ac pulvinar felis quam sit.</p>
+                    <label>¿Qué formas de pago hay disponibles en Global Arte?</label>
+                    <p>El pago debe realizarse mediante transferencia bancaria al cerrar el contrato de venta o mediante reembolso al recibo de la mercancía.</p>
                 </section>
 
                 <section class="toggle">
-                    <label>Curabitur eget leo at velit imperdiet vague iaculis vitaes?</label>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eget leo at velit imperdiet varius. In eu ipsum vitae velit congue iaculis vitae at risus. Nullam tortor nunc, bibendum vitae semper a, volutpat eget massa. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer fringilla, orci sit amet posuere auctor, orci eros pellentesque odio, nec pellentesque erat ligula nec massa. Aenean consequat lorem ut felis ullamcorper posuere gravida tellus faucibus. Maecenas dolor elit, pulvinar eu vehicula eu, consequat et lacus. Duis et purus ipsum. In auctor mattis ipsum id molestie. Donec risus nulla, fringilla a rhoncus vitae, semper a massa. Vivamus ullamcorper, enim sit amet consequat laoreet, tortor tortor dictum urna, ut egestas urna ipsum nec libero. Nulla justo leo, molestie vel tempor nec, egestas at massa. Aenean pulvinar, felis porttitor iaculis pulvinar, odio orci sodales odio, ac pulvinar felis quam sit.</p>
+                    <label>¿Cuáles son los gastos de envío?</label>
+                    <p>Los gastos de envío dependen del tamaño, formato de la obra elegida y el lugar donde deban enviarse, y se le informará siempre antes de cerrar el contrato. Cada obra llevará sus gastos de envío independientemente del número de obras compradas en cada pedido.</p>
                 </section>
 
                 <section class="toggle">
-                    <label>Curabitur eget leo at velit imperdiet viaculis vitaes?</label>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eget leo at velit imperdiet varius. In eu ipsum vitae velit congue iaculis vitae at risus. Nullam tortor nunc, bibendum vitae semper a, volutpat eget massa.</p>
+                    <label>¿Cuál es el plazo de entrega?</label>
+                    <p>El plazo de entrega contará a partir de la fecha de recepción del pago y dependerá del producto elegido. Si deseas un envío urgente, infórmanos para que podamos presupuestarlo.</p>
                 </section>
 
                 <section class="toggle">
-                    <label>Curabitur eget leo at velit imperdiet varius iaculis vitaes?</label>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eget leo at velit imperdiet varius. In eu ipsum vitae velit congue iaculis vitae at risus. Nullam tortor nunc, bibendum vitae semper a, volutpat eget massa. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer fringilla, orci sit amet posuere auctor, orci eros pellentesque odio, nec pellentesque erat ligula nec massa. Aenean consequat lorem ut felis ullamcorper posuere gravida tellus faucibus. Maecenas dolor elit, pulvinar eu vehicula eu, consequat et lacus. Duis et purus ipsum. In auctor mattis ipsum id molestie. Donec risus nulla, fringilla a rhoncus vitae, semper a massa. Vivamus ullamcorper, enim sit amet consequat laoreet, tortor tortor dictum urna, ut egestas urna ipsum nec libero. Nulla justo leo, molestie vel tempor nec, egestas at massa. Aenean pulvinar, felis porttitor iaculis pulvinar, odio orci sodales odio, ac pulvinar felis quam sit.</p>
+                    <label>¿Puedo hacer un cuadro personalizado?</label>
+                    <p>Claro, en Global Arte somos especialistas en la personalización del producto. Póngase en contacto con nuestro departamento técnico y ellos le asesorarán adecuadamente.</p>
                 </section>
 
                 <section class="toggle">
-                    <label>Curabitur eget leo at velit imperdiet vague iaculis vitaes?</label>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eget leo at velit imperdiet varius. In eu ipsum vitae velit congue iaculis vitae at risus. Nullam tortor nunc, bibendum vitae semper a, volutpat eget massa. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer fringilla, orci sit amet posuere auctor, orci eros pellentesque odio, nec pellentesque erat ligula nec massa. Aenean consequat lorem ut felis ullamcorper posuere gravida tellus faucibus. Maecenas dolor elit, pulvinar eu vehicula eu, consequat et lacus. Duis et purus ipsum. In auctor mattis ipsum id molestie. Donec risus nulla, fringilla a rhoncus vitae, semper a massa. Vivamus ullamcorper, enim sit amet consequat laoreet, tortor tortor dictum urna, ut egestas urna ipsum nec libero. Nulla justo leo, molestie vel tempor nec, egestas at massa. Aenean pulvinar, felis porttitor iaculis pulvinar, odio orci sodales odio, ac pulvinar felis quam sit.</p>
+                    <label>¿Cuántas obras existen de cada original?</label>
+                    <p>En el apartado obras de nuestra página web, podrá ver el stock de cada una de las obras. La que usted adquiera estará numerada en función del total de stock a venta y siempre respetamos ese número. Cada obra estará autentificada con la firma del artista.</p>
                 </section>
 
                 <section class="toggle">
-                    <label>Curabitur eget leo at velit imperdiet viaculis vitaes?</label>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eget leo at velit imperdiet varius. In eu ipsum vitae velit congue iaculis vitae at risus. Nullam tortor nunc, bibendum vitae semper a, volutpat eget massa.</p>
+                    <label>¿Está incluido el IVA?</label>
+                    <p>Todas nuestras obras llevan el IVA incluido en su precio final.</p>
                 </section>
 
                 <section class="toggle">
-                    <label>Curabitur eget leo at velit imperdiet varius iaculis vitaes?</label>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eget leo at velit imperdiet varius. In eu ipsum vitae velit congue iaculis vitae at risus. Nullam tortor nunc, bibendum vitae semper a, volutpat eget massa. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer fringilla, orci sit amet posuere auctor, orci eros pellentesque odio, nec pellentesque erat ligula nec massa. Aenean consequat lorem ut felis ullamcorper posuere gravida tellus faucibus. Maecenas dolor elit, pulvinar eu vehicula eu, consequat et lacus. Duis et purus ipsum. In auctor mattis ipsum id molestie. Donec risus nulla, fringilla a rhoncus vitae, semper a massa. Vivamus ullamcorper, enim sit amet consequat laoreet, tortor tortor dictum urna, ut egestas urna ipsum nec libero. Nulla justo leo, molestie vel tempor nec, egestas at massa. Aenean pulvinar, felis porttitor iaculis pulvinar, odio orci sodales odio, ac pulvinar felis quam sit.</p>
+                    <label>¿Puedo devolver el producto?</label>
+                    <p>Sí, por supuesto, siempre tiene derecho de devolver el producto durante 5 días hábiles desde la recepción de la mercancía. Tiene más información sobre el modo de devolución en las condiciones de contratación.</p>
+                </section>
+
+                <section class="toggle">
+                    <label>¿Qué ocurre si el producto viene roto o dañado?</label>
+                    <p>Al recibo del producto, debe examinarlo para asegurarse de que ha llegado en perfectas condiciones y de no ser así, hay un plazo máximo de 48 horas para contactar con Global Arte e informar del problema para proceder a solucionarlo.</p>
                 </section>
             </div>
 
