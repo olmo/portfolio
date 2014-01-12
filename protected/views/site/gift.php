d8532ea28866b7cf071a0fd8f21b8cf1defd28ea
Francisco Carranza <oz.carranza@gmail.com>
2014-01-12 23:13:47 +0100

    reglito

--- protected/views/site/gift.php
+++ protected/views/site/gift.php
@@ -55,59 +55,8 @@ $this->breadcrumbs=array(
     <h3 class="short"><strong>Cuatro </strong> cheques</h3>
 
     <div class="row">
-
-        <div class="pricing-table">
-            <div class="span3">
-                <div class="plan">
-                    <h3>Básico<span>59 €</span></h3>
-                    <?php echo CHtml::link('Realizar pedido',array('site/contact')); ?>
-                    <ul>
-                        <li><b>10</b> Lore ipsum</li>
-                        <li><b>100</b> Lore ipsum</li>
-                        <li><b>30</b> Lore ipsum</li>
-                        <li><b>10</b> Lore ipsum</li>
-                    </ul>
-                </div>
-            </div>
-            <div class="span3 center">
-                <div class="plan most-popular">
-                    <div class="plan-ribbon-wrapper"><div class="plan-ribbon">Popular</div></div>
-                    <h3>Profesional<span>29 €</span></h3>
-                    <?php echo CHtml::link('Realizar pedido',array('site/contact')); ?>
-                    <ul>
-                        <li><b>10</b> Lore ipsum</li>
-                        <li><b>100</b> Lore ipsum</li>
-                        <li><b>30</b> Lore ipsum</li>
-                        <li><b>10</b> Lore ipsum</li>
-                    </ul>
-                </div>
-            </div>
-            <div class="span3">
-                <div class="plan">
-                    <h3>Estándar<span>17 €</span></h3>
-                    <?php echo CHtml::link('Realizar pedido',array('site/contact')); ?>
-                    <ul>
-                        <li><b>10</b> Lore ipsum</li>
-                        <li><b>100</b> Lore ipsum</li>
-                        <li><b>30</b> Lore ipsum</li>
-                        <li><b>10</b> Lore ipsum</li>
-                    </ul>
-                </div>
-            </div>
-            <div class="span3">
-                <div class="plan">
-                    <h3>Mixto<span>9 €</span></h3>
-                    <?php echo CHtml::link('Realizar pedido',array('site/contact')); ?>
-                    <!-- <a class="btn btn-large btn-primary" href="#">Realizar pedido</a> -->
-                    <ul>
-                        <li><b>10</b> Lore ipsum</li>
-                        <li><b>100</b> Lore ipsum</li>
-                        <li><b>30</b> Lore ipsum</li>
-                        <li><b>10</b> Lore ipsum</li>
-                    </ul>
-                </div>
-            </div>
-        </div>
+        <a href="<?php echo Yii::app()->request->baseUrl; ?>/images/gift/bono.jpg">
+        <img src="<?php echo Yii::app()->request->baseUrl; ?>/images/gift/bono.jpg" alt="">
     </div>
 
     <hr class="tall" />
